2732ab142f2bb2b40a0e207ea7b27376c9818c0b
devashish223 <129968942+devashish223@users.noreply.github.com>
2023-04-17 03:59:02 +0530

    Time: 3 ms (70.96%), Space: 5.8 MB (94.11%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0,b = 1;
+        if(n==0||n==1){
+            return n;
+        }
+        int c = 0;
+        for(int i=2;i<=n;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        
+        return b;
+    }
+};
