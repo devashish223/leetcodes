23fb5f9454c3e286230090fbbb32fcbc77d0f3ff
devashish223 <129968942+devashish223@users.noreply.github.com>
2023-06-01 21:12:28 +0530

    Time: 3 ms (36.81%), Space: 6 MB (74.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int climbStairs(int n) {
+        int dp[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        if(n<=1){
+            return 1;
+        }
+        for(int i=2;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2]);
+        }
+        return dp[n];
+    }
+};
